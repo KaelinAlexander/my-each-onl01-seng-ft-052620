b8803cddb29d7951dfb648322ec11335e331f6e5
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-05-29 20:54:15 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,7 +1,7 @@
 def my_each(array)
 i=0
 while i < array.length
-yield(array[i]
+yield(array[i])
 i = i + 1
 puts array
 end
@@ -9,4 +9,6 @@ end
 
 my_each([i]) do |set|
   new_array >> set
+  new_array
+  puts new_array
 end
